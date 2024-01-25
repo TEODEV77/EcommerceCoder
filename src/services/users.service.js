3bb8142e9f0bcf70a32a804a30594fda39c8598b
Mateo <mateo.orolot@gmail.com>
2024-01-25 10:54:33 -0500

    Add User Repository

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,26 +1,26 @@
-import UserDao from "../dao/user.dao.js";
+import { userRepository } from "../repositories/index.js";
 import CartsService from "./carts.service.js";
 
 export default class UsersService {
   static create(payload) {
-    return UserDao.create(payload);
+    return userRepository.dao.create(payload);
   }
 
   static findByEmail(email) {
-    return UserDao.findByEmail(email);
+    return userRepository.dao.findByEmail(email);
   }
 
   static async createUserCart(payload) {         
     const shopping_cart = await CartsService.create();
     payload.cart = shopping_cart._id;
-    return UserDao.create(payload);          
+    return userRepository.dao.create(payload);          
   }
 
   static updateById(id, payload) {
-    return UserDao.updateById(id, payload); 
+    return userRepository.dao.updateById(id, payload); 
   }
 
   static deleteById(id) {
-    return UserDao.deleteById(id);
+    return userRepository.dao.deleteById(id);
   }
 }
