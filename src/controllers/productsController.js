b4fb43f2d33f9425d184dae869794e32d82eb0c8
Mateo <mateo.orolot@gmail.com>
2024-03-01 21:28:28 -0500

    Refactor products controller

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,3 +1,4 @@
+import { checkOwner } from "../logic/product.logic.js";
 import { productsService } from "../services/index.service.js";
 
 const getAllProducts = async (req, res, next) => {
@@ -25,6 +26,78 @@ const getAllProducts = async (req, res, next) => {
   }
 };
 
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  const query = { _id: id };
+  try {
+    const product = await productsService.getBy(query);
+    res.status(200).json({ status: "success", payload: product });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+    next(error);
+  }
+};
+
+const create = async (req, res, next) => {
+  const {
+    title,
+    code,
+    category,
+    description,
+    stock,
+    price,
+    status,
+    thumbnails = [],
+  } = req.body;
+  const owner = req.user.email;
+  const payload = {
+    title,
+    code,
+    owner,
+    category,
+    description,
+    stock,
+    price,
+    status,
+    thumbnails,
+  };
+  try {
+    const product = await productsService.save(payload);
+    res.status(201).json({ status: "success", payload: product });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+    next(error);
+  }
+};
+
+const update = async (req, res, next) => {
+  const { id } = req.params;
+  const { title, category, description, stock, price, status } = req.body;
+  const payload = {
+    title,
+    category,
+    description,
+    stock,
+    price,
+    status,
+  };
+  try {
+    const {role,email} = req.user;
+    const checkOwn = await checkOwner({role,email,id});
+    if(!checkOwn){
+      return res.status(403).json({ status: "error", message: "Unauthorized" });
+    }
+    const product = await productsService.update(id, payload);
+    return res.status(200).json({ status: "success", payload: product });
+  } catch (error) {
+    res.status(500).json({ message: error });
+    next(error);
+  }
+};
+
 export default {
   getAllProducts,
+  getById,
+  create,
+  update,
 };
--- src/routers/api/products.router.js
+++ src/routers/api/products.router.js
@@ -1,9 +1,28 @@
 import { Router } from "express";
 import productsController from "../../controllers/productsController.js";
 
+import {
+  authMiddlewarePassport,
+  authorizeMiddlewarePassport,
+} from "../../config/middleware/authMiddleware.js";
+
 const router = Router();
 
+router.get("/:id", productsController.getById);
 router.get("/", productsController.getAllProducts);
 
+router.post(
+  "/",
+  authMiddlewarePassport("jwt"),
+  authorizeMiddlewarePassport(["admin", "premium"]),
+  productsController.create
+);
+
+router.put(
+  "/:id",
+  authMiddlewarePassport("jwt"),
+  authorizeMiddlewarePassport(["admin", "premium"]),
+  productsController.update
+);
 
 export default router;
