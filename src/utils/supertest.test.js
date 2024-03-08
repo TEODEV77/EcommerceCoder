cd924ca3156c80ca53485d56aabb1d50d17254d2
Mateo <mateo.orolot@gmail.com>
2024-03-08 15:29:38 -0500

    Product tests

--- src/utils/supertest.test.js
+++ src/utils/supertest.test.js
@@ -1,46 +1,93 @@
 import { expect } from "chai";
 import supertest from "supertest";
-import SingletonEnvironment from "../env/singletonEnvironment.js";
-
-import { flags } from "../utils.js";
-
-const { environment } = SingletonEnvironment.getInstance(flags.environ);
-const { host, port } = environment.env.api;
-
-const local = `${host}:${port}/`;
 
 const requester = supertest(`http://127.0.0.1:7070`);
 
-describe("Users API", () => {
-  it("should return a token", async () => {
-    const userMock = {
-      email: "user300@gmail.com",
-      password: "123",
-    };
-    const { statusCode, body, ok } = await requester
-      .post("/api/sessions/auth/login")
-      .send(userMock);
-    expect(statusCode).to.equal(200);
-    expect(ok).to.be.ok;
-    expect(body).to.have.property("status").to.equal("success");
-    expect(body).to.have.property("payload").to.be.an("string");
-  });
+describe("Auth tests", () => {
+  let cookie;
 
-  it("should create an user", async () => {
-    const userMock = {
-      firstName: "User 701",
-      lastName: "700",
-      email: "user701@gmail.com",
-      password: "123",
-      age: 701,
-    };
-    const { statusCode, body, ok } = await requester
-      .post("/api/sessions/auth/register")
-      .send(userMock);
-    expect(statusCode).to.equal(201);
-    expect(ok).to.be.ok;
-    expect(body).to.have.property("status").to.equal("success");
-    expect(body).to.have.property("payload").to.be.an("object");
+  describe("Session tests", () => {
+    it("(Sign-up) - Should create an user", async () => {
+      const userMock = {
+        firstName: "User 707",
+        lastName: "707",
+        email: "user707@gmail.com",
+        password: "123",
+        age: 701,
+      };
+      const { statusCode, body, ok } = await requester
+        .post("/api/sessions/auth/register")
+        .send(userMock);
+      expect(statusCode).to.equal(201);
+      expect(ok).to.be.ok;
+      expect(body).to.have.property("status").to.equal("success");
+      expect(body).to.have.property("payload").to.be.an("object");
+    });
+
+    it("(Sign-in) - Should return a token", async () => {
+      const userMock = {
+        email: "user300@gmail.com",
+        password: "123",
+      };
+      const { statusCode, headers, body, ok } = await requester
+        .post("/api/sessions/auth/login")
+        .send(userMock);
+      expect(statusCode).to.equal(200);
+      expect(ok).to.be.ok;
+      expect(body).to.have.property("status").to.equal("success");
+      expect(body).to.have.property("payload").to.be.an("string");
+
+      const [key, value] = headers["set-cookie"][0].split("=");
+      cookie = { key, value };
+    });
+
+    describe("Product tests", () => {
+      it("should create a product successfully", async () => {
+        const { statusCode, body, ok } = await requester
+          .post("/api/dev/products")
+          .set("Cookie", [`${cookie.key}=${cookie.value}`])
+          .send({
+            title: "Test Product 7",
+            code: "TST-PRD 7",
+            category: "Test",
+            owner: "user300@gmail.com",
+            description: "This is a test product 7",
+            stock: 60,
+            price: 600,
+            status: true,
+            thumbnails: ["test5.jpg"],
+          });
+        expect(statusCode).to.equal(201);
+        expect(ok).to.be.ok;
+        expect(body).to.have.property("status").to.equal("success");
+        expect(body).to.have.property("payload").to.have.property("_id");
+      });
+
+      it("should update a product successfully", async () => {
+        const { statusCode, ok } = await requester
+          .put("/api/dev/products/65ea9222e6ea0214bfd55081")
+          .set("Cookie", [`${cookie.key}=${cookie.value}`])
+          .send({
+            title: "Test Product Updated",
+  
+            category: "Test",
+            description: "This is a test product",
+            stock: 10,
+            price: 100,
+            status: true,
+          });
+        expect(statusCode).to.equal(200);
+        expect(ok).to.be.ok;
+      });
+
+      it("should delete a product successfully", async () => {
+        const { statusCode, ok } = await requester
+          .delete("/api/dev/products/65eb72a390c9c2adc362cecc")
+          .set("Cookie", [`${cookie.key}=${cookie.value}`]);
+        expect(statusCode).to.equal(200);
+        expect(ok).to.be.ok;
+      });
+    });
   });
 
   it("(Wrong credentials) - should return an error", async () => {
@@ -54,9 +101,10 @@ describe("Users API", () => {
     expect(statusCode).to.equal(400);
     expect(body).to.have.property("status").to.equal("error");
   });
+
 });
 
-describe("Carts API", () => {
+describe("Cart API", () => {
   it("should return a list of carts", async () => {
     const { statusCode, body, ok } = await requester.get("/api/dev/carts");
     expect(statusCode).to.equal(200);
@@ -66,14 +114,7 @@ describe("Carts API", () => {
   });
 });
 
-describe("Products API", () => {
-  before(function () {
-    this.cookie = {
-      key: "token",
-      value:
-        "eyJmaXJzdE5hbWUiOiJVc2VyIDMwMCIsImxhc3ROYW1lIjoiMzAwIiwidXNlcm5hbWUiOiJObyB1c2VybmFtZSIsInByb3ZpZGVyIjoiTm8gcHJvdmlkZXIiLCJyb2xlIjoicHJlbWl1bSIsImVtYWlsIjoidXNlcjMwMEBnbWFpbC5jb20iLCJhZ2UiOjMwMCwiY2FydCI6IjY1ZTI1ODg0M2M1Mzg4MWU4MmQ2Y2EzZSIsInR5cGUiOiJhdXRoIiwiaWF0IjoxNzA5NzQ3MDQwLCJleHAiOjE3MDk3NTQyNDB9",
-    };
-  });
+describe("Product API", () => {
   it("should return a list of products", async () => {
     const { statusCode, body, ok } = await requester.get("/api/dev/products");
     expect(statusCode).to.equal(200);
@@ -106,66 +147,4 @@ describe("Products API", () => {
     expect(statusCode).to.equal(404);
     expect(body).to.have.property("status").to.equal("error");
   });
-
-  // it("should create a product successfully", async () => {
-  //   const cookie = {
-  //     key: "token",
-  //     value:
-  //       "eyJmaXJzdE5hbWUiOiJVc2VyIDMwMCIsImxhc3ROYW1lIjoiMzAwIiwidXNlcm5hbWUiOiJObyB1c2VybmFtZSIsInByb3ZpZGVyIjoiTm8gcHJvdmlkZXIiLCJyb2xlIjoicHJlbWl1bSIsImVtYWlsIjoidXNlcjMwMEBnbWFpbC5jb20iLCJhZ2UiOjMwMCwiY2FydCI6IjY1ZTI1ODg0M2M1Mzg4MWU4MmQ2Y2EzZSIsInR5cGUiOiJhdXRoIiwiaWF0IjoxNzA5NzQ3MDQwLCJleHAiOjE3MDk3NTQyNDB9",
-  //   };
-
-  //   const { statusCode, body, ok } = await requester
-  //     .post("/api/dev/products")
-  //     .set("Cookie", [`${cookie.key}=${cookie.value}`])
-  //     .send({
-  //       title: "Test Product",
-  //       code: "TST-PRD",
-  //       category: "Test",
-  //       description: "This is a test product",
-  //       stock: 10,
-  //       price: 100,
-  //       status: "active",
-  //       thumbnails: ["test.jpg"],
-  //     });
-  //   expect(statusCode).to.equal(201);
-  //   expect(ok).to.be.ok;
-  //   expect(body).to.have.property("status").to.equal("success");
-  //   expect(body).to.have.property("payload").to.have.property("_id");
-  // });
-
-  // it("should update a product successfully", async () => {
-  //   const { statusCode, body, ok } = await requester
-  //     .put("/api/dev/products/6558dd604e0a87e8653355f1")
-  //     .set("Cookie", [`${this.cookie.key}=${this.cookie.value}`])
-  //     .send({
-  //       title: "Test Product Updated",
-  //       code: "TST-PRD-UPD",
-  //       category: "Test",
-  //       description: "This is a test product",
-  //       stock: 10,
-  //       price: 100,
-  //       status: "active",
-  //       thumbnails: ["test.jpg"],
-  //     });
-  //   expect(statusCode).to.equal(200);
-  //   expect(ok).to.be.ok;
-  //   expect(body).to.have.property("status").to.equal("success");
-  //   expect(body)
-  //     .to.have.property("payload")
-  //     .to.have.property("_id")
-  //     .to.equal("6558dd604e0a87e8653355f1");
-  // });
-
-  // it("should delete a product successfully", async () => {
-  //   const { statusCode, body, ok } = await requester
-  //     .delete("/api/dev/products/6558dd604e0a87e8653355f1")
-  //     .set("Cookie", `${this.cookie.value}=${this.cookie.value}`);
-  //   expect(statusCode).to.equal(200);
-  //   expect(ok).to.be.ok;
-  //   expect(body).to.have.property("status").to.equal("success");
-  //   expect(body)
-  //     .to.have.property("payload")
-  //     .to.have.property("_id")
-  //     .to.equal("6558dd604e0a87e8653355f1");
-  // });
 });
